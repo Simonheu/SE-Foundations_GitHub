ae8cccfd272d500a2ee683d119e0fa90f567f96e
Simonheu <s.heu@outlook.de>
2019-04-11 10:24:30 +0200

    Backup from yesterday

---
+++ final_project/static/myscripts.js
@@ -0,0 +1,10 @@
+
+// Menu Burger Animation adopted from FreeCodeCamp: https://www.w3schools.com/howto/howto_css_menu_icon.asp
+function menuAnimation(x) {
+    x.classList.toggle("change");
+}
+
+// Menu Overlay 
+function togglenav(x){
+    x.classList.toggle("open");
+}
