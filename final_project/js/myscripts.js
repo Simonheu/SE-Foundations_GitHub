95e8c1c75620882034bfee02ec36ceb1ab8e853b
Simonheu <s.heu@outlook.de>
2019-04-23 15:06:08 +0200

    styling

--- final_project/js/myscripts.js
+++ final_project/js/myscripts.js
@@ -1,4 +1,6 @@
 
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+
 
 // Menu Burger Animation adopted from FreeCodeCamp: https://www.w3schools.com/howto/howto_css_menu_icon.asp
 function menuAnimation(x) {
@@ -27,25 +29,12 @@ function togglediv(t){
 }
 
 // Mouse Wheel Function from https://codepen.io/chriscoyier/pen/WmyKWO
-//Modiefied and Helped by Janek Fischer
-//this function checks where in the body the user is and scrolls to right or left when hovering over the right section
-$(function() {
-    $('.capturescroll').mousewheel(function(event, delta) { //mousewhweel is an eventhandler //event is the default event (scrolling up/down// delta is the mouse up or down scroll
-        const speed = 3
-        const mousescroll = $('#horizontalscroll')
-        const scrollable = mousescroll[0] //jquery array 
-        const scrollTopPosition = mousescroll.offset().top //position of horizontal scroll component
-        const scrollTopPadding = Math.abs(delta) * speed //gives padding for better usability //buffer
-        const scrollTop = $('body')[0].scrollTop - delta //checks where the user is in the body
-        const width = scrollable.scrollWidth - mousescroll.width() - speed //width of combonanant minnus visible width - buffer
-        if(scrollable.scrollLeft <= width && delta < 0 && scrollTop >= scrollTopPosition - scrollTopPadding //for scrolling down
-            || scrollable.scrollLeft > 0 && delta > 0  && scrollTop <= scrollTopPosition + scrollTopPadding){ //for scrolling up
-            scrollable.scrollLeft -= (delta * speed); //executes scrolling 
-            event.preventDefault(); //prevents event from default up or down scroll
-            $('body')[0].scrollTop = scrollTopPosition //snap vertical scroll
-        }
-    });
-});
+// $(document).ready(function() {
+//     $('mousescroll').mousewheel(function(e, delta) {
+//     this.scrollLeft -= (delta);
+//     e.preventDefault();
+//     });
+// });
 
 // document.getElementById("mousescroll").onscroll = function() {mouseWheel()};
 
