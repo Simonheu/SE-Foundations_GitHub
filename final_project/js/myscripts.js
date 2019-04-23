258ff4511564b6888ab2eccad09ed0edf13c9a41
Simonheu <s.heu@outlook.de>
2019-04-23 15:10:25 +0200

    setup

--- final_project/js/myscripts.js
+++ final_project/js/myscripts.js
@@ -1,4 +1,3 @@
-
 src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
 
 
@@ -27,6 +26,24 @@ function togglediv(t){
         x.style.display = "none";
     }
 }
+
+$(function() {
+    $('.capturescroll').mousewheel(function(event, delta) { //mousewhweel is an eventhandler //event is the default event (scrolling up/down// delta is the mouse up or down scroll
+        const speed = 3
+        const mousescroll = $('#horizontalscroll')
+        const scrollable = mousescroll[0] //jquery array
+        const scrollTopPosition = mousescroll.offset().top //position of horizontal scroll component
+        const scrollTopPadding = Math.abs(delta) * speed //gives padding for better usability //buffer
+        const scrollTop = $('body')[0].scrollTop - delta //checks where the user is in the body
+        const width = scrollable.scrollWidth - mousescroll.width() - speed //width of combonanant minnus visible width - buffer
+        if(scrollable.scrollLeft <= width && delta < 0 && scrollTop >= scrollTopPosition - scrollTopPadding //for scrolling down
+            || scrollable.scrollLeft > 0 && delta > 0  && scrollTop <= scrollTopPosition + scrollTopPadding){ //for scrolling up
+            scrollable.scrollLeft -= (delta * speed); //executes scrolling
+            event.preventDefault(); //prevents event from default up or down scroll
+            $('body')[0].scrollTop = scrollTopPosition //snap vertical scroll
+        }
+    });
+ });
 
 // Mouse Wheel Function from https://codepen.io/chriscoyier/pen/WmyKWO
 // $(document).ready(function() {
